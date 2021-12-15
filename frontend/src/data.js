e4f984dbc2576fb7312d00b4d56ff031a99cb003
mike_hall_dev <mihall6846@gmail.com>
2021-12-14 21:36:38 -0600

    Initial Setup

---
+++ frontend/src/data.js
@@ -0,0 +1,41 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Test Coffee',
+            category: 'Coffee',
+            image: '/images/img_1.jpeg',
+            price: 10.99,
+            countInStock: 10,
+            brand: 'Default',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'good coffee',
+        },
+        {
+            _id: '2',
+            name: 'Test Coffee 2',
+            category: 'Coffee2',
+            image: '/images/img_2.jpeg',
+            price: 12.99,
+            countInStock: 10,
+            brand: 'Default',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'good coffee2',
+        },
+        {
+            _id: '3',
+            name: 'Test Coffee 3',
+            category: 'Coffee2',
+            image: '/images/img_3.jpeg',
+            price: 9.99,
+            countInStock: 10,
+            brand: 'Default',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'good coffee3',
+        }
+    ]
+};
+export default data;
